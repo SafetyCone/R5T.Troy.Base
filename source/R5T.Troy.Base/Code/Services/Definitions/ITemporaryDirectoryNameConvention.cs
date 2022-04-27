c789ccbddcb48fbe808d3b0588db4accd5dd310f
davidcoats <david@getsafetycone.com>
2022-04-27 15:37:15 -0700

    Interim changes.

--- source/R5T.Troy.Base/Code/Services/Definitions/ITemporaryDirectoryNameConvention.cs
+++ source/R5T.Troy.Base/Code/Services/Definitions/ITemporaryDirectoryNameConvention.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;using R5T.T0064;
 
 
 namespace R5T.Troy
-{
-    public interface ITemporaryDirectoryNameConvention
+{[ServiceDefinitionMarker]
+    public interface ITemporaryDirectoryNameConvention:IServiceDefinition
     {
         string GetTemporaryDirectoryName();
     }
